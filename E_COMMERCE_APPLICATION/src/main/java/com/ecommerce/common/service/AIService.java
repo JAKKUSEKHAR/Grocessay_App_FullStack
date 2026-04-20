c1f8845a586439e1374f21a650579593180d653a
JAKKUSEKHAR <jakkusekhar22@gmail.com>
2026-04-20 16:26:33 +0530

    deploy ready2

--- E_COMMERCE_APPLICATION/src/main/java/com/ecommerce/common/service/AIService.java
+++ E_COMMERCE_APPLICATION/src/main/java/com/ecommerce/common/service/AIService.java
@@ -30,12 +30,16 @@ public class AIService {
         """.formatted(prompt);
 
         return webClient.post()
-        .uri("/v1beta/models/gemini-pro:generateContent?key=" + apiKey)
+        .uri("/v1beta/models/gemini-1.5-flash:generateContent?key=" + apiKey)
         .header("Content-Type", "application/json")
         .bodyValue(requestBody)
         .retrieve()
         .bodyToMono(JsonNode.class)
         .map(json -> {
+
+            // 🔍 log full response (temporary)
+            System.out.println("FULL RESPONSE: " + json);
+
             if (json.has("candidates")) {
                 return json
                         .get("candidates")
@@ -45,9 +49,12 @@ public class AIService {
                         .get(0)
                         .get("text")
                         .asText();
+            } else if (json.has("error")) {
+                return "Gemini Error: " + json.get("error").toString();
             } else {
-                return "Error: " + json.toString();
+                return "Unexpected response: " + json.toString();
             }
+
         })
         .block();
     }
